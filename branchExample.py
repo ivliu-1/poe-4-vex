538479fe264181fbe5a8979a50bcb0f8f220c64b
mergegi <128318777+mergegi@users.noreply.github.com>
2026-05-04 13:56:33 -0400

    Create branchExample.py

---
+++ branchExample.py
@@ -0,0 +1,10 @@
+"""
+Name:           branchExample.py    
+Author(s):      Maggie Chen and Ivy Liu 
+Description:    Demonstrate how to use Github branches
+Date:           5/4/26
+"""
+
+counter = 0
+
+while(counter < 10)
